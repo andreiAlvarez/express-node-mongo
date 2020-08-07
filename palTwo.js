68ce8d9befb482102a4ce8e7d3e847a17c3c1785
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-08-06 22:33:29 -0400

    Create palTwo.js

---
+++ palTwo.js
@@ -0,0 +1,11 @@
+function palindromeTwo(str) {
+  return (
+    str.replace(/[\W_]/g, "").toLowerCase() ===
+    str
+      .replace(/[\W_]/g, "")
+      .toLowerCase()
+      .split("")
+      .reverse()
+      .join("")
+  );
+}
